5f22779742903592d372bec6650b194591f01053
Shristi Singh <shtisti911892@gmail.com>
2025-02-24 20:19:12 +0530

    1st commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 console.log("Script running...")
 document.querySelector('.cross').style.display='none';
 document.querySelector('.hamburger').addEventListener("click", ()=>{
@@ -15,4 +16,23 @@ document.querySelector('.hamburger').addEventListener("click", ()=>{
         },300);
     }
     
+=======
+console.log("Script running...")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display ='inline'
+        document.querySelector('.cross').style.display='none'
+         
+    }
+    else{
+        document.querySelector('.ham').style.display ='none'
+        setTimeout(() =>{
+            document.querySelector('.cross').style.display='inline'  
+
+        },300);
+    }
+    
+>>>>>>> 3e21915 (1st commit)
 })
